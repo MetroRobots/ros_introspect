23eabeb3a4756954bcfa27aec516e37e2d6fed70
David V. Lu!! <davidvlu@gmail.com>
2023-11-30 14:16:56 -0500

    ROS Package Finder (#2)

---
+++ src/ros_introspect/finder.py
@@ -0,0 +1,76 @@
+REPO_MARKERS = ['.git', '.svn', 'fake_git_root']
+
+
+def is_repo_root(folder):
+    # Look for repo metadata (or fake_git_root for testing purposes)
+    for marker in REPO_MARKERS:
+        if (folder / marker).exists():
+            return True
+    return False
+
+
+def is_repo_marker(folder):
+    return folder.name in REPO_MARKERS
+
+
+def find_repo_root(path):
+    if path.is_file():
+        path = path.parent
+
+    while not is_repo_root(path):
+        parent_dir = path.parent
+        if path == parent_dir:
+            raise RuntimeError(f'Cannot find repo root: {path}')
+
+        path = parent_dir
+    return path
+
+
+def is_package_root(folder):
+    if not (folder / 'package.xml').exists():
+        return False
+
+    if (folder / 'COLCON_IGNORE').exists():
+        return False
+
+    return True
+
+
+def find_package_roots(root_folder):
+    if is_package_root(root_folder):
+        yield root_folder
+    else:
+        for subfolder in root_folder.iterdir():
+            if subfolder.is_dir() and not is_repo_marker(subfolder):
+                yield from find_package_roots(subfolder)
+
+
+def find_sibling_package_roots(package_folder):
+    parent_path = package_folder.parent
+    for subfolder in parent_path.iterdir():
+        if not subfolder.is_dir():
+            continue
+        if is_package_root(subfolder) and subfolder != package_folder:
+            yield subfolder
+
+
+def walk(root, include_hidden=True):
+    """Yield the paths of all the "valid" files in a directory, relative to the directory"""
+
+    queue = [root]
+    while queue:
+        folder = queue.pop(0)
+        for subpath in folder.iterdir():
+            if not include_hidden and subpath.name.startswith('.'):
+                # Ignore hidden files and directories
+                continue
+
+            if subpath.is_dir():
+                # Folder
+                if not is_repo_marker(subpath):
+                    queue.append(subpath)
+            else:
+                # File
+                if subpath.suffix == '.pyc' or subpath.suffix.endswith('~'):
+                    continue
+                yield subpath.relative_to(root)
--- test/test_finder.py
+++ test/test_finder.py
@@ -1,10 +1,40 @@
 import pathlib
-import ros_introspect
+import pytest
+from ros_introspect.finder import find_package_roots, is_repo_root, find_repo_root, find_sibling_package_roots, walk
 
 THIS_FILE = pathlib.Path(__file__)
 ROOT_FOLDER = THIS_FILE.parent.parent
+TEST_DATA_FOLDER = ROOT_FOLDER / 'test_data'
 
 
 def test_package_finder():
-    print(ROOT_FOLDER)
-    print(ros_introspect)
+    roots = list(find_package_roots(TEST_DATA_FOLDER))
+    assert len(roots) == 4
+
+
+def test_repo_root():
+    assert not is_repo_root(THIS_FILE)
+    assert not is_repo_root(THIS_FILE.parent)
+    assert is_repo_root(ROOT_FOLDER)
+
+    assert find_repo_root(THIS_FILE) == ROOT_FOLDER
+    with pytest.raises(RuntimeError):
+        find_repo_root(pathlib.Path('/tmp'))
+
+
+def test_siblings():
+    waymond = TEST_DATA_FOLDER / 'waymond'
+    w_sibs = list(find_sibling_package_roots(waymond))
+    assert len(w_sibs) == 0
+
+    hibachi = TEST_DATA_FOLDER / 'eleanor' / 'eeaao'
+    e_sibs = list(find_sibling_package_roots(hibachi))
+    assert len(e_sibs) == 2
+    names = [sib.name for sib in e_sibs]
+    assert 'kungfu' in names
+
+
+def test_walk():
+    folder = TEST_DATA_FOLDER / 'eleanor' / 'kungfu'
+    assert len(list(walk(folder))) == 5
+    assert len(list(walk(folder, include_hidden=False))) == 4
